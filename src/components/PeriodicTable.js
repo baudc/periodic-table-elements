81cb5d766613efa3945a990e19a8287c8c75c111
Cesar Baud <baud.cesar@gmail.com>
2021-06-02 00:59:55 +0200

    integrated button component

--- src/components/PeriodicTable.js
+++ src/components/PeriodicTable.js
@@ -1,28 +1,16 @@
 import React from "react";
 import "./PeriodicTable.css";
 import JsonData from "../assets/elements.json";
+import Button from "./Button";
 
 const PeriodicTable = () => {
   const elements = JsonData.elements;
   return (
     <div className="table-container">
       <div id="grid">
-        {elements.map((item) => {
-          return (
-            <div
-              className="element"
-              style={{
-                backgroundColor: "#" + item["cpk-hex"],
-                gridColumnStart: item.xpos,
-                gridRowStart: item.ypos,
-              }}>
-              <div className="element-number">{item.number}</div>
-              <div className="element-symbol">{item.symbol}</div>
-              <div className="element-name">{item.name}</div>
-              <div className="element-phase">{item.phase}</div>
-            </div>
-          );
-        })}
+        {elements.map((item) => (
+          <Button item={item} />
+        ))}
       </div>
     </div>
   );
