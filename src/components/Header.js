80406d56b11b819ca06eddf98153e3b8dbfe4365
Cesar Baud <baud.cesar@gmail.com>
2021-06-01 18:01:19 +0200

    split code into components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <p>Tableau periodique des élements</p>
+    </header>
+  );
+}
+ 
+export default Header;
